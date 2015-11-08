e132ef776f7522b82931466f2b20296a7fbdc58f
Nat Weerawan <nat@opendream.co.th>
2015-11-08 14:12:07 +0800

    ESPert compatible.

--- ESP8266_Weather_Station/ESP8266_Weather_Station.ino
+++ ESP8266_Weather_Station/ESP8266_Weather_Station.ino
@@ -11,15 +11,10 @@
 #include <ESP_Adafruit_SSD1306.h>
 /*===== DHT22 =====*/
 #include "DHT.h"
-#include <OneWire.h>
 #include <ESP8266WiFi.h>
 /*===== END INCLUDE =====*/
 
 /*===== START DEFINE =====*/
-
-#define power_oled 0
-#define power_dht22 13
-#define read_pir 12
 
 /*===== DISPLAY =====*/
 #define OLED_RESET 4
@@ -28,10 +23,10 @@ Adafruit_SSD1306 display(OLED_RESET);
 #define XPOS 0
 #define YPOS 1
 #define DELTAY 2
-#define LOGO16_GLCD_HEIGHT 16 
-#define LOGO16_GLCD_WIDTH  16 
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
 /*===== DHT22 =====*/
-#define DHTPIN 2     // what pin we're connected to
+#define DHTPIN 12     // what pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
 #define DEBUG
 #define DEBUG_PRINTER Serial
@@ -51,7 +46,6 @@ DHT *dht;
 
 void initDht(DHT **dht, uint8_t pin, uint8_t dht_type);
 void readDht(DHT *dht, float *temp, float *humid);
-void uploadThingsSpeak(float t, float );
 /*===== DISPLAY =====*/
 static const unsigned char logo16_glcd_bmp[] =
 { B00000000, B11000000,
@@ -69,7 +63,8 @@ static const unsigned char logo16_glcd_bmp[] =
   B00111111, B11110000,
   B01111100, B11110000,
   B01110000, B01110000,
-  B00000000, B00110000 };
+  B00000000, B00110000
+};
 #if (SSD1306_LCDHEIGHT != 64)
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
@@ -86,128 +81,125 @@ int t = 10;
 int v = 0;
 /*=====================*/
 
-const char* ssid     = "noe";
-const char* pass     = "12345678";
+const char* ssid     = "NAT.WRTNODE";
+const char* pass     = "devicenetwork";
 
 WiFiConnector wifi(ssid, pass);
 
 void init_wifi()
 {
-    wifi.on_connecting([](const void* message)
-    {
-        showDHT();
-    });
+  wifi.on_connecting([](const void* message)
+  {
+    Serial.println("CONNECTING...");
+    showDHT();
+  });
 
-    wifi.on_connected([](const void* message)
-    {
-        Serial.println ((char*)message);
-        Serial.println("IP address: ");
-        Serial.println(WiFi.localIP());        
-    });
+  wifi.on_connected([](const void* message)
+  {
+    Serial.println ((char*)message);
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+  });
 
-//    wifi.begin();
+  //    wifi.begin();
 }
 
 
 void setup() {
-    pinMode(power_oled, OUTPUT);
-    digitalWrite(power_oled, HIGH);
-    delay(10);
-    /*===== DISPLAY =====*/
-    
-    display.begin(SSD1306_SWITCHCAPVCC, 0x78>>1);
-    display.println("begin");
-    display.display();
-//    display.begin(SSD1306_SWITCHCAPVCC, 0x3C>>1);
-    
-    Serial.begin(115200);
+  Serial.println("BEGIN");
+  Serial.begin(115200);
+  delay(10);
+  /*===== DISPLAY =====*/
 
-    /*=== power ===*/
+  display.begin(SSD1306_SWITCHCAPVCC, 0x78 >> 1);
+  display.println("begin");
+  display.display();
 
-    pinMode(power_dht22, OUTPUT);
-    pinMode(read_pir, INPUT);
+  
 
-    digitalWrite(power_dht22, HIGH);
-    
-    delay(10);
+  /*=== power ===*/
 
-    init_wifi();
 
-    initDht(&dht, DHTPIN, DHTTYPE);
+
+  delay(30);
+  
+  init_wifi();
+
+  initDht(&dht, DHTPIN, DHTTYPE);
 
 }
 
 void print_display(String text) {
-      display.clearDisplay();
-      display.setTextSize(3);
-      display.setTextColor(WHITE);
-      display.setCursor(2,20);
-      display.clearDisplay();
-      
-      display.print(text.c_str());
-      
-      display.println();
-      display.display();  
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(2, 20);
+  display.clearDisplay();
+
+  display.print(text.c_str());
+
+  display.println();
+  display.display();
 }
 
 void loop() {
-    static float t_dht;
-    static float h_dht;
+  static float t_dht;
+  static float h_dht;
 
-    readDht(dht, &t_dht, &h_dht);
+  readDht(dht, &t_dht, &h_dht);
 
-    if (millis() % 1000 == 0) {
-      DEBUG_PRINTLN(t_dht);;
-    }
-    
-    
-    showDHT();
+  if (millis() % 1000 == 0) {
+    DEBUG_PRINTLN(t_dht);;
+  }
 
-//    wifi.loop();
+
+  showDHT();
+
+  wifi.loop();
 }
 
 
 void initDht(DHT **dht, uint8_t pin, uint8_t dht_type) {
-    *dht = new DHT(pin, dht_type, 30);
-    (*dht)->begin();
-    DEBUG_PRINTLN(F("DHTxx test!"))  ;
+  *dht = new DHT(pin, dht_type, 30);
+  (*dht)->begin();
+  DEBUG_PRINTLN(F("DHTxx test!"))  ;
 }
 
 
 void readDht(DHT *dht, float *temp, float *humid) {
 
-    if (dht == NULL) {
-        DEBUG_PRINTLN(F("[dht22] is not initialised. please call initDht() first."));
-        return;
-    }
+  if (dht == NULL) {
+    DEBUG_PRINTLN(F("[dht22] is not initialised. please call initDht() first."));
+    return;
+  }
 
-    // Reading temperature or humidity takes about 250 milliseconds!
-    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-    float h = dht->readHumidity();
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht->readHumidity();
 
-    // Read temperature as Celsius
-    float t = dht->readTemperature();
-    // Read temperature as Fahrenheit
-    float f = dht->readTemperature(true);
+  // Read temperature as Celsius
+  float t = dht->readTemperature();
+  // Read temperature as Fahrenheit
+  float f = dht->readTemperature(true);
 
-    // Check if any reads failed and exit early (to try again).
-    if (isnan(h) || isnan(t) || isnan(f)) {
-        DEBUG_PRINTLN("Failed to read from DHT sensor!");
-        return;
-    }
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    DEBUG_PRINTLN("Failed to read from DHT sensor!");
+    return;
+  }
 
-    // Compute heat index
-    // Must send in temp in Fahrenheit!
-    float hi = dht->computeHeatIndex(f, h);
-    *temp = t;
-    *humid = f;
+  // Compute heat index
+  // Must send in temp in Fahrenheit!
+  float hi = dht->computeHeatIndex(f, h);
+  *temp = t;
+  *humid = f;
 }
 
 /* DISPLAY */
 
 void showDHT(void) {
-      float t = dht->readTemperature();
-      float h = dht->readHumidity();
-    
-      print_display(String(t-7) + String(" C"));
+  float t = dht->readTemperature();
+  float h = dht->readHumidity();
+
+  print_display(String(t) + String(" C"));
 }
